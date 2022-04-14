c8f2736241a138ae5bdab837c643f89a5160f014
Egon Willighagen <egon.willighagen@gmail.com>
2022-04-14 20:11:12 +0200

    Added various other used ontologies

---
+++ org.pathvisio.io.rdf/src/main/java/org/pathvisio/io/rdf/ontologies/CITO.java
@@ -0,0 +1,21 @@
+/* Copyright (C) 2021  Egon Willighagen <egonw@users.sf.net>
+ *
+ * License: new BSD.
+ */
+package org.pathvisio.io.rdf.ontologies;
+
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.ResourceFactory;
+
+public class CITO {
+
+	public static final String URI =
+			"http://purl.org/spar/cito/";
+
+	private static final Property property(String local) {
+		return ResourceFactory.createProperty(URI, local);
+	}
+
+	public static final Property cites = property("cites"); // has attribute
+
+}
